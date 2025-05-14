fc4a3472b8db4e8b1f18673601be330b87ce17df
Igor Strelnyk <59284261+rodriguezz4060@users.noreply.github.com>
2025-05-14 20:58:01 +0300

    create prisma seed

--- src/app/class-guides/page.tsx
+++ src/app/class-guides/page.tsx
@@ -1,9 +1,8 @@
-import { Container } from "@root/components/shared";
-import GuideButton from "@root/components/shared/guide-button";
-import CreateGuideModal from "@root/components/shared/guide/create-guide/create-guide-modal";
-import { GuideData } from "./_actions/create-guide";
-import { prisma } from "@prisma/prisma-client";
-import { notFound } from "next/navigation";
+import { Container } from '@root/components/shared';
+import GuideButton from '@root/components/shared/guide-button';
+import CreateGuideModal from '@root/components/shared/guide/create-guide/create-guide-modal';
+import { GuideData } from './_actions/create-guide';
+import { prisma } from '@prisma/prisma-client';
 
 export default async function Page() {
   const guides = await prisma.guide.findMany({
@@ -18,17 +17,16 @@ export default async function Page() {
     },
   });
 
-  if (!guides.length) {
-    return notFound();
-  }
-
   const initialData = await GuideData();
+
   return (
-    <>
-      <Container className="dark:bg-zinc-900">
-        <CreateGuideModal initialData={initialData} />
+    <Container className='dark:bg-zinc-900'>
+      <CreateGuideModal initialData={initialData} />
+      {guides.length === 0 ? (
+        <p>Гайды пока не созданы. Нажмите кнопку, чтобы добавить новый гайд.</p>
+      ) : (
         <GuideButton guides={guides} />
-      </Container>
-    </>
+      )}
+    </Container>
   );
 }
